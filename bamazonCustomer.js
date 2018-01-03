96b6ed96fd738e5ce187b370512e8f293d6cd69c
fernandofuentes <fernando.fuentes1113@gmail.com>
2018-01-02 20:32:01 -0600

    updates

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,15 +12,16 @@ var total = (price * quantityChosen);
 
 // CONNECT TO SERVER / MYSQL
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'me',
-  password : 'secret',
-  database : 'my_db'
+  port: "3000"
+  host: 'localhost',
+  user: 'root',
+  password: 'ugcNHrgm4Y.r',
+  database: 'bamazon'
 });
 
 connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+connection.query('SELECT 1 + 1 AS solution', function(error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);
 });
